49eb1496a0acb131a8fda571847d7d94a0fdeb10
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2023-07-10 16:38:28 -0300

    Remove leftover code

--- js1/script.js
+++ js1/script.js
@@ -1,9 +1,3 @@
-// const loreal = document.getElementById("loreal");
-// loreal.innerHTML = "Loreal is <strong>nigh</strong>."
-
-// const cont = document.querySelectorAll(".container");
-// console.log(cont)
-
 window.alert('Hello, world!')
 window.confirm('Click OK if you wish to proceed')
 window.prompt('Enter your name to continue')
