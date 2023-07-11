08c66e0e06e28ca96a2b01b39c04ea0903d4d6f6
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2023-07-11 14:41:55 -0300

    Exercise 3

---
+++ ex003/script.js
@@ -0,0 +1,5 @@
+var n1 = Number(window.prompt('Enter the first number'))
+var n2 = Number(window.prompt('Enter the second number'))
+var r = n1 + n2
+// window.alert(r + ' is the number you\'re looking for.')
+window.alert(`${r} is the number you're looking for. (${n1} + ${n2})`)
