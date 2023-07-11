4d13de8fd992f50234db5df7d79d3ab42d074640
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2023-07-11 14:42:00 -0300

    Exercise 4

---
+++ ex004/script.js
@@ -0,0 +1,3 @@
+var user = window.prompt('Enter your name to continue')
+document.write(`Welcome, ${user}. Your name has ${user.length} characters.<br>`)
+document.write(`Your name is ${user.toUpperCase()} in <strong>uppercase</strong> and ${user.toLowerCase()} in <strong>lowercase</strong>.`)
