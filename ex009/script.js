5797b54e52b29ae6d6cdf5712529562bfe87c061
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2023-07-15 13:26:42 -0300

    Exercise 9

---
+++ ex009/script.js
@@ -0,0 +1,8 @@
+var userInput = 'france'
+var country = userInput.toUpperCase()
+console.log(`Country of origin: ${country}.`)
+if (country == 'USA') {
+    console.log('US Citizen.')
+} else {
+    console.log('Non-US Citizen.')
+}
