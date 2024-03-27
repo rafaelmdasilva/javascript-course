d028f97d5dea13c95808d2877f05ffa37cbe96a4
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2024-03-27 15:01:02 -0300

    Exercise 21

---
+++ ex021/script.js
@@ -0,0 +1,11 @@
+function parity(num) {
+    if (num % 2 == 0) {
+        return ('even!')
+    } else {
+        return ('odd!')
+    }
+}
+
+// let result = parity(2)
+// console.log(result)
+console.log(parity(11))
